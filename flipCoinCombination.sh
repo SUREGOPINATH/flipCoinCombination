949fa967adbae9c01a9ccb136d92462f1aff8f20
SURE GOPINATH <suregopinath@gmail.com>
2020-10-08 13:58:11 +0530

    flipping coin display head or tail is done

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,4 +1,24 @@
 #!/bin/bash -x
 echo "Welcome to flipCoinCombination"
 
+function tossChecker() {
 
+	toss=$((RANDOM%2))
+	echo $toss
+}
+
+function flipCoinCombination() {
+
+	heads=0
+	tails=1
+	toss=$(tossChecker)
+	case $toss in
+		0)
+			echo "heads"
+			;;
+		1)
+			echo "tails"
+			;;
+	esac
+}
+flipCoinCombination
