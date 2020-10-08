5bfde023eb17b81c823f463db29113655d2421aa
SURE GOPINATH <suregopinath@gmail.com>
2020-10-08 14:50:19 +0530

    the percentage of singlet is done

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,5 +1,9 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "Welcome to flipCoinCombination"
+
+declare -A dictionary
+dictionary[H]=0
+dictionary[T]=0
 
 function tossChecker() {
 
@@ -7,18 +11,41 @@ function tossChecker() {
 	echo $toss
 }
 
+function storeInDictionary() {
+
+	dictionary[$1]=$((${dictionary[$1]}+1))
+}
+
+function percentage() {
+
+	hPercentage=$((${dictionary[H]}*100 /5))
+	tPercentage=$((${dictionary[T]}*100 /5))
+	echo "The Percentage of Heads is : "$hPercentage"%"
+	echo "The Percentage of Tails is : "$tPercentage"%"
+}
 function flipCoinCombination() {
 
 	heads=0
 	tails=1
-	toss=$(tossChecker)
-	case $toss in
-		0)
-			echo "heads"
-			;;
-		1)
-			echo "tails"
-			;;
-	esac
+	count=0
+	echo "The tosses are : "
+	while [ $count -lt 5 ]
+	do
+		toss=$(tossChecker)
+		case $toss in
+			0)
+				echo "heads"
+				storeInDictionary H
+				;;
+			1)
+				echo "tails"
+				storeInDictionary T
+				;;
+		esac
+		count=$(($count+1))
+	done
+	percentage 
 }
 flipCoinCombination
+echo ${!dictionary[@]}
+echo ${dictionary[@]}
